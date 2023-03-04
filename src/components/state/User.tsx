99972e6c7a7eaaa6806a4c31ddcf81bb043f3003
Muhammad Saleh <ranaasaleh846@gmail.com>
2023-03-04 08:18:36 +0500

    Step 6 -- How to work with useState hook

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,7 +6,12 @@ type AuthUser = {
 };
 
 const User = () => {
+	// How to handle the situation when initial value's type is different from future value's type
 	const [user, setUser] = useState<AuthUser | null>(null);
+
+	// In a case, where we know that `AuthUser` will be available as soon as component mounts and it will not be null then there is another solution for this
+	// const [user, setUser] = useState<AuthUser>({} as AuthUser);
+	// Here we are lieing  to the TypeScript compiler that `{}` is of type AuthUser
 
 	const handleLogin = () => {
 		setUser({
@@ -22,6 +27,8 @@ const User = () => {
 		<div>
 			<button onClick={handleLogin}>Login</button>
 			<button onClick={handleLogout}>Logout</button>
+
+			{/* We have to use optional chaining because `user` can be `null` */}
 			<div>User name is {user?.name}</div>
 			<div>User email is {user?.email}</div>
 		</div>
